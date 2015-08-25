46f98174f8a12a8662841b8c9943872deb9e0833
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-08-25 21:14:59 +0200

    Generate cpu monitor source tree

---
+++ monitor/logger/py_cpu_monitor/CPUMonitor.py
@@ -0,0 +1,30 @@
+
+
+
+
+class CPUMonitor:
+    'CPUMonitor class'
+
+
+
+    def __init__(self, processes):
+        self.cpuValues # float, LinkedList
+        self.processes = processes # string, LinkedList
+        self.cpuCounter = PerformanceCounter() # PerformanceCounter, LinkedList
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ monitor/logger/py_cpu_monitor/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['DiskResource.py',
+           'MemoryMonitor.py',
+           'MonitorResource.py',
+           'SocketListener.py',
+           'Monitor.py',
+           'CPUMonitor.py']
+
+import DiskResource, MemoryMonitor, MonitorResource, SocketListener, Monitor, CPUMonitor
