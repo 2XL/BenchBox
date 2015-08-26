16f626282989d3e008c6949e62ae243b1e4ad79d
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-08-26 16:45:49 +0200

    Added Disk Resource Caputure

--- monitor/logger/py_cpu_monitor/DiskResource.py
+++ monitor/logger/py_cpu_monitor/DiskResource.py
@@ -1,8 +1,68 @@
 
+from Diagnostics import PerformanceCounter
 from MonitorResource import MonitorResource
+import os
+import threading
+
+
+class DriveInfo():
+    name = None
+    IsReady = False
+    def __init__(self):
+        print ''
+
+    def GetDrives(self):
+        print ''
+
+    def name(self):
+        return 'c'
+
+    def IsReady(self):
+        return True
+
+    def TotalFreeSpace(self):
+        return 100
+
+
 
 class DiskResource(MonitorResource):
     ''
 
+    allDrives = DriveInfo() # DriveInfo
+    diskValues = list() # list {float}
+
     def __init__(self):
         print 'constructor'
+
+        self.allDrives.GetDrives()
+
+    def prepareMonitoring(self):
+        self.diskValues = list() # float
+
+
+    def captureValue(self):
+        for d in self.allDrives: # DriveInfo
+            if 'c' in d.Name: # en aquest cas ... xD ke faig???
+                print 'success'
+
+            if d.IsReady == True:
+                self.diskValues.AddLast(d.TotalFreeSpace)
+
+    def saveResults(self, filename):
+        # open a file
+        file = os.open('disk_' + filename, os.O_APPEND|os.O_CREAT) # maybe it has to be append instead of crete and
+        # write
+        os.write(file, str(os.getpid()))
+        for it in self.cpuValues: # float iterator
+            while it:
+                os.write(file, it.pop(1))
+        os.close(file)
+
+
+
+if __name__ == '__main__':
+    disk = DiskResource()
+    for x in range(100):
+        disk.captureValue()
+        threading.sleep(100)
+
