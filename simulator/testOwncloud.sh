3307fc2790a389272fcde8482824f55c850eaa39
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-09-21 17:27:14 +0200

    Added simple simulator test scripts

---
+++ simulator/testOwncloud.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "simple test owncloud"
+ ./executor.py -o 20 -p sync -t 1 -f owncloud_folder -x OwnCloud
---
+++ simulator/testStacksync.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "simple test stacksync"
+ ./executor.py -o 20 -p sync -t 1 -f stacksync_folder -x StackSync
