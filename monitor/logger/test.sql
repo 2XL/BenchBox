2ce07a9a6d8a9a00d7b67a23391ea3421210ee3a
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-09-21 10:56:44 +0200

    Change logger structure, adding tables logger_cpu, .._ram, .._net, .._hdd, .._id

--- monitor/logger/test.sql
+++ monitor/logger/test.sql
@@ -18,6 +18,80 @@ len int
 
 
 
+
+
+
+-- -----------------------------------
+-- CPU
+drop table if exists logger_cpu;
+create table if not exists logger_cpu (
+  ts timestamp,
+  cpu_usage int,
+  cpu_count int,
+  testid bigint
+) stored as parquet;
+
+-- RAM
+drop table if exists logger_ram;
+create table if not exists logger_ram (
+  ts timestamp,
+  ram_usage int,
+  ram_count int,
+  testid bigint
+) stored as parquet;
+
+-- NET
+drop table if exists logger_net;
+create table if not exists logger_net (
+  ts timestamp,
+  up_size int,
+  down_size int,
+  testid bigint
+)stored as parquet;
+
+-- HDD
+drop table if exists logger_hdd;
+
+create table if not exists logger_hdd (
+  ts timestamp,
+  data_write int,
+  data_read int,
+  testid bigint
+) stored as parquet;
+
+-- ID
+drop table if exists logger_id;
+create table if not exists logger_id(
+  ts timestamp,
+  pc_client_name string,
+  pc_server_id string,
+  profile string,
+  test_id bigint,
+  test_definition string
+) stored as parquet;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # create logger table with more detailed fields
 # can handle comparison of cpu usage between machines
 
@@ -28,12 +102,10 @@ create table if not exists logger_cpu (
   ts timestamp,
   cpu_usage int, # 1-100 cpu usage
   cpu_count int, # number of cpu
-  pc_client_name string, # stacksync|owncloud
-  pc_server_ip string, # ip
-  profile string, # sync|cdn|backup|regular|idle
-  testid bigint, # current timestamp
-  testdefine string, # define the test, can be string serialized (object) to be parsed...
-)
+
+  testid bigint # current timestamp
+
+) stored as parquet;
 
 
 # can handle comparison of ram usage between machines
@@ -44,6 +116,8 @@ create table if not exists logger_ram (
   ts timestamp,
   ram_usage int, # in bytes of usage
   ram_count int, # total of ram
+
+  testid bigint # current timestamp
 )
 
 # can handle comparison of network traffic usage as absolute value
@@ -54,7 +128,9 @@ create table if not exists logger_net (
   ts timestamp,
   up_size int, # in bytes
   down_size int, # in bytes
-)
+
+  testid bigint # current timestamp
+)stored as parquet;
 
   # the comparison between two timestamps, can be
   # difference between usage epoch
@@ -69,7 +145,9 @@ create table if not exists logger_ssd (
   ts timestamp,
   data_write int,
   data_read int,
-)
+
+  testid bigint # current timestamp
+)stored as parquet;
 
   # it makes no sense...
 
@@ -89,12 +167,14 @@ create table if not exists logger_ssd (
 
 # create a table defining the test, test definition table
 
-drop table if not exists logger_id(
+drop table if exists logger_id;
+
+create table if not exists logger_id(
 ts timestamp,
 pc_client_name string,
 pc_server_id string,
 profile string, # sync|cdn|backup|regular|idle
 test_id bigint, # current timestamp, absolute precision
-test_definition string
-)
+test_definition string # # define the test, can be string serialized (object) to be parsed...
+) stored as parquet;
 
