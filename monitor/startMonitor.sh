1a4de98d96647bfedbcd32ffbf8d9d7f8b2832de
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-10-03 19:13:15 +0200

    Fix sudo permision to louch SocketListener

--- monitor/startMonitor.sh
+++ monitor/startMonitor.sh
@@ -3,7 +3,8 @@
 
 echo 'start socketListener';  
 if [ -d ~/monitor ]; then  
-cd ~/monitor/logger/py_cpu_monitor && python SocketListener.py;  
+cd ~/monitor/logger/py_cpu_monitor
+echo 'vagrant' | sudo -S python SocketListener.py;
 echo 'stacksync client and monitor started';  
 else  
 echo 'stacksync client monitoring not available';  
